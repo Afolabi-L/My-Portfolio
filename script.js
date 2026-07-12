0f28016b59a44a4a4b2a0f7dc15edfca6dc9fbc3
Afolabi.L <encrypt7me0@gmail.com>
2026-07-12 11:37:33 +0100

    Add button press effect to primary and ghost buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+const btns = document.querySelectorAll('.btn-primary, .btn-ghost');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.add('btn-pressed');
+        setTimeout(() => {
+            btn.classList.remove('btn-pressed');
+        }, 400);
+    });
+});
